70c3d0b53392eb092b0a5b8474cf6a2512e41dea
DenysKors <105918404+DenysKors@users.noreply.github.com>
2022-11-15 16:48:09 +0200

    Gallery fix simplelightbox

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { fetchImages } from "./fetchImages";
 import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const refs = {
     searchForm: document.querySelector('#search-form'),
